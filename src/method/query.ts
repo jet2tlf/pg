16f1005ddcb7806effad44feda0570f4082c7377
Natan Fernandes <natanfernandes@protonmail.com>
2024-04-08 06:10:55 -0300

    fix: sql injection

--- src/method/query.ts
+++ src/method/query.ts
@@ -1,18 +1,10 @@
 import { pgClient } from '../database/index';
-import { parseArguments } from '../utils/parseArguments';
 
-export async function query(query: string, parameters: any[], callback: (result: any) => void) {
-    try {
-        [query, parameters] = parseArguments(query, parameters);
-        
-        const result = await pgClient.query(query);
-
-        return callback ? callback(result.rows[0]) : result.rows[0];
-    } catch (error) {
-        throw new Error(`Error executing query: ${error}`);
-    }
-}
-
-function print(text: any): any {
-    console.log(text);
+export async function query(query: string, parameters: unknown[], callback: (result: any) => void) {
+  try {        
+    const result = await pgClient.query(query, parameters);
+    return callback ? callback(result.rows[0]) : result.rows[0];
+  } catch (error) {
+    throw new Error(`Error executing query: ${error}`);
+  }
 }
